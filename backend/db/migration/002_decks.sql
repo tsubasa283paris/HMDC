1e56fa12727587eb2e31056ef0f994c71e17740d
tsubasa283paris <tsubasa283paris@gmail.com>
2022-04-02 02:27:46 +0900

    Add table decks

---
+++ backend/db/migration/002_decks.sql
@@ -0,0 +1,19 @@
+-- +migrate Up
+CREATE TABLE decks (
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(id)
+            ON DELETE SET NULL
+);
+
+
+-- +migrate Down
+DROP TABLE decks;
