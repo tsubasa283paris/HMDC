70697e1ecd3150d786852b287901ad278842c307
tsubasa283paris <tsubasa283paris@gmail.com>
2022-04-02 02:57:51 +0900

    Add initial and generated files of sqlc

---
+++ backend/db/query/users.sql
@@ -0,0 +1,29 @@
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 AND deleted_at IS NULL;
+
+-- name: ListUsers :many
+SELECT (id, name) FROM users
+WHERE deleted_at IS NULL
+ORDER BY id;
+
+-- name: CreateUser :one
+INSERT INTO users (
+  id, password, name
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: UpdateUser :exec
+UPDATE users
+SET id = $2,
+    password = $3,
+    name = $4,
+    updated_at = NOW()
+WHERE id = $1 AND deleted_at IS NULL;
+
+-- name: DeleteUser :exec
+UPDATE users
+SET deleted_at = NOW()
+WHERE id = $1;
