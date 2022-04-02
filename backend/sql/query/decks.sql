2d61642ae783b4ab376f422aef9784110437ae80
tsubasa283paris <tsubasa283paris@gmail.com>
2022-04-02 20:18:18 +0900

    Add queries for decks

---
+++ backend/sql/query/decks.sql
@@ -0,0 +1,128 @@
+-- name: GetDeck :one
+SELECT
+    d.id,
+    d.name,
+    d.description,
+    d.user_id,
+    l.league_id
+FROM decks d
+LEFT JOIN league_decks l ON d.id = l.deck_id
+WHERE d.id = $1 AND d.deleted_at IS NULL
+AND   NOT EXISTS (
+    SELECT 1
+    FROM league_decks l2
+    WHERE l2.deck_id = l.deck_id
+    AND   l2.created_at > l.created_at
+);
+
+-- name: UpdateDeck :exec
+UPDATE decks
+SET name = $2,
+    description = $3,
+    updated_at = NOW()
+WHERE id = $1 AND deleted_at IS NULL;
+
+-- name: GetDeckStats :many
+SELECT
+    l.id AS league_id,
+    (SELECT (
+        (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_1_id = $1 AND dl.league_id = l.id
+        ) + (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_2_id = $1 AND dl.league_id = l.id
+        )
+    )) AS num_duel,
+    (SELECT (
+        (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_1_id = $1 AND dl.result = 1 AND dl.league_id = l.id
+        ) + (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_2_id = $1 AND dl.result = 2 AND dl.league_id = l.id
+        )
+    )) AS num_win
+FROM leagues l;
+
+-- name: ListDecks :many
+SELECT
+    d.id,
+    d.name,
+    d.description,
+    d.user_id,
+    l.league_id,
+    (SELECT (
+        (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_1_id = d.id
+        ) + (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_2_id = d.id
+        )
+    )) AS num_duel,
+    (SELECT (
+        (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_1_id = d.id AND dl.result = 1
+        ) + (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_2_id = d.id AND dl.result = 2
+        )
+    )) AS num_win
+FROM decks d
+LEFT JOIN league_decks l ON d.id = l.deck_id
+WHERE d.deleted_at IS NULL
+AND   NOT EXISTS (
+    SELECT 1
+    FROM league_decks l2
+    WHERE l2.deck_id = l.deck_id
+    AND   l2.created_at > l.created_at
+);
+
+-- name: ListDecksByUser :many
+SELECT
+    d.id,
+    d.name,
+    d.description,
+    d.user_id,
+    l.league_id,
+    (SELECT (
+        (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_1_id = d.id
+        ) + (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_2_id = d.id
+        )
+    )) AS num_duel,
+    (SELECT (
+        (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_1_id = d.id AND dl.result = 1
+        ) + (
+            SELECT COUNT(*)
+            FROM duels dl
+            WHERE dl.deck_2_id = d.id AND dl.result = 2
+        )
+    )) AS num_win
+FROM decks d
+LEFT JOIN league_decks l ON d.id = l.deck_id
+WHERE d.user_id = $1 AND d.deleted_at IS NULL
+AND   NOT EXISTS (
+    SELECT 1
+    FROM league_decks l2
+    WHERE l2.deck_id = l.deck_id
+    AND   l2.created_at > l.created_at
+);
