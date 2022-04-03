e5353cf5f016582dad9cc81e567f971bcbe81ba9
tsubasa283paris <tsubasa283paris@gmail.com>
2022-04-03 22:14:25 +0900

    Add query needed for /api/users/:userId/duelhistory API

--- backend/sql/query/users.sql
+++ backend/sql/query/users.sql
@@ -67,3 +67,45 @@ SELECT
         )
     )) AS num_win
 FROM leagues l;
+
+-- name: GetUserDuelHistory :many
+SELECT
+    dl1.id,
+    dl1.league_id,
+    dl1.user_2_id AS opponent_user_id,
+    dl1.deck_1_id AS deck_id,
+    dl1.deck_2_id AS opponent_deck_id,
+    (SELECT
+        CASE
+            WHEN dl1.result = -1 THEN '-'
+            WHEN dl1.result = 0 THEN 'draw'
+            WHEN dl1.result = 1 THEN 'win'
+            WHEN dl1.result = 2 THEN 'lose'
+            ELSE 'undefined'
+        END
+    ) AS result,
+    dl1.created_at
+FROM duels dl1
+WHERE dl1.user_1_id = $1
+    AND dl1.confirmed_at IS NOT NULL
+UNION
+SELECT
+    dl2.id,
+    dl2.league_id,
+    dl2.user_1_id AS opponent_user_id,
+    dl2.deck_2_id AS deck_id,
+    dl2.deck_1_id AS opponent_deck_id,
+    (SELECT
+        CASE
+            WHEN dl2.result = -1 THEN '-'
+            WHEN dl2.result = 0 THEN 'draw'
+            WHEN dl2.result = 1 THEN 'lose'
+            WHEN dl2.result = 2 THEN 'win'
+            ELSE 'undefined'
+        END
+    ) AS result,
+    dl2.created_at
+FROM duels dl2
+WHERE dl2.user_2_id = $1
+    AND dl2.confirmed_at IS NOT NULL
+ORDER BY created_at;
